b220eafa031747cb9a4935342b448dc6b1512923
Deven Kaushik <devenkaushik16@gmail.com>
2024-08-12 16:22:36 +0530

    screenshots uploaded

--- src/com/shashi/servlets/AdminLogoutServlet.java
+++ src/com/shashi/servlets/AdminLogoutServlet.java
@@ -17,25 +17,33 @@ import com.shashi.utility.TrainUtil;
 @WebServlet("/adminlogout")
 public class AdminLogoutServlet extends HttpServlet {
 
-	/**
-	 * 
-	 * @param req
-	 * @param res
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.setContentType("text/html");
-		PrintWriter pw = res.getWriter();
-		if (TrainUtil.isLoggedIn(req, UserRole.ADMIN)) {
-			TrainUtil.logout(res);
-			RequestDispatcher rd = req.getRequestDispatcher("AdminLogin.html");
-			rd.include(req, res);
-			pw.println("<div class='tab'><p1 class='menu'>You have been successfully logged out !</p1></div>");
-		} else {
-			RequestDispatcher rd = req.getRequestDispatcher("AdminLogin.html");
-			rd.include(req, res);
-			pw.println("<div class='tab'><p1 class='menu'>You are Already Logged Out !</p1></div>");
-		}
-	}
+    /**
+     * 
+     * @param req
+     * @param res
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+        
+        // Add CSS styles
+        pw.println("<style>");
+        pw.println("body, html { height: 100%; margin: 0; display: flex; justify-content: center; align-items: center; font-family: Arial, sans-serif; }");
+        pw.println(".tab { display: flex; justify-content: center; align-items: center; height: 100%; }");
+        pw.println(".menu { font-size: 24px; font-weight: bold; color: white; background-color: orange; padding: 20px; border-radius: 10px; text-align: center; }");
+        pw.println("</style>");
+        
+        if (TrainUtil.isLoggedIn(req, UserRole.ADMIN)) {
+            TrainUtil.logout(res);
+            RequestDispatcher rd = req.getRequestDispatcher("AdminLogin.html");
+            rd.include(req, res);
+            pw.println("<div class='tab'><p class='menu'>You have been successfully logged out!</p></div>");
+        } else {
+            RequestDispatcher rd = req.getRequestDispatcher("AdminLogin.html");
+            rd.include(req, res);
+            pw.println("<div class='tab'><p class='menu'>You are already logged out!</p></div>");
+        }
+    }
 }
